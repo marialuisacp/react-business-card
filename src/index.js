37f3e2d3205a3be4d800a8f0e3d82169480a2334
Maria Luísa <marialuisa@MacBook-Air-de-Maria.local>
2019-11-15 16:34:29 -0300

    Melhor escrita do codigo

--- src/index.js
+++ src/index.js
@@ -7,20 +7,15 @@ import List from './screens/list/List';
 import Item from './screens/item/Item';
 import { HashRouter, Route, Switch } from "react-router-dom";
 
-// import { Provider } from 'react-redux';
-// import { Store } from './store';
-
 import { createBrowserHistory } from 'history';
 const history = createBrowserHistory()
 
 ReactDOM.render(
   <HashRouter basename='/' history={history}>
-    {/* <Provider store={Store}> */}
     <Switch>
       <Route exact path="/" component={List} />
       <Route path="/letter" component={Item} />
     </Switch>
-    {/* </Provider> */}
   </HashRouter>
   , document.getElementById('root'));
 

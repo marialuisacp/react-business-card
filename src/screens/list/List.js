600f77bb497f4b884ea166138af24d524de2191f
Maria Luísa <marialuisa@MacBook-Air-de-Maria.local>
2019-11-15 21:32:15 -0300

    Alfabeto

--- src/screens/list/List.js
+++ src/screens/list/List.js
@@ -1,132 +1,93 @@
-import React, { Component } from 'react'
-import NonPassiveTouchTarget from '../../touch/NonPassiveTouchTarget'
-import TouchCarousel, { clamp, range } from '../../touch'
-import touchWithMouseHOC from '../../touch/TouchWithMouseHOC'
+import React, { Component } from 'react';
+import { CarouselProvider, Dot, ButtonNext, ButtonBack, Slide, Slider } from 'pure-react-carousel';
+import Header from '../../components/header/Header';
 
-import Header from '../../components/header/Header'
-
-import './List.scss'
-
-const data = range(0, 999).map(n => ({
-  title: `Card ${n}`,
-  text: `This is card ${n}`
-}))
-const queryPage = parseInt(window.location.search.slice(1))
-const startPage = queryPage ? clamp(queryPage, 0, data.length - 1) : 0
-
-const carouselWidth = clamp(window.innerWidth, 0, 900)
-const cardSize = carouselWidth
+import './List.scss';
+import '../../styles/styles.scss';
+import '../../styles/lib/react-carousel.es.css';
 
 class List extends Component {
-  constructor(props) {
-    super(props)
-    const renderedData = this.getRenderedData(startPage)
-    this.state = {
-      page: clamp(startPage, 0, data.length - renderedData.length),
-      renderedData
-    }
-    this.defaultCursor = (data.length - renderedData.length) - startPage
-  }
-
-  // Open a length-3 window on the data
-  getRenderedData(cursor) {
-    switch (cursor) {
-      case 0: {
-        return data.slice(0, 3)
-      }
-      case data.length - 1: {
-        return data.slice(data.length - 3)
-      }
-      default: {
-        return data.slice(cursor - 1, cursor + 2)
-      }
-    }
-  }
-
-  modPage = () => {
-    const cursor = this.carousel.getCursor()
-    const { page, renderedData } = this.state
-    if (Math.round(cursor) !== cursor) return
-    if ( // Do we reach the edge of the window but not the edge of the data?
-      (page !== 0 && cursor === 0) ||
-      (page !== data.length - renderedData.length && cursor === 1 - renderedData.length)
-    ) {
-      // Then move the window.
-      const newCursor = -1 // put cursor at center
-      const newPage = page - cursor + newCursor
-      this.setState({
-        page: newPage,
-        renderedData: this.getRenderedData(newPage)
-      })
-      // This kinda breaks grabbing. But not a big deal I guess.
-      this.carousel.modAs(newCursor)
-    }
-  }
-
-  container = touchWithMouseHOC((props) => {
-    const { cursor, carouselState, ...rest } = props
-    const translateX = cursor * cardSize
-    return (
-      <NonPassiveTouchTarget
-        className='carousel-container'
-        onTouchStart={this.modPage}
-        onMouseDown={this.modPage}
-      >
-        <NonPassiveTouchTarget
-          className='carousel-track'
-          style={{ transform: `translate3d(${translateX}px, 0, 0)` }}
-          {...rest}
-        />
-      </NonPassiveTouchTarget>
-    )
-  })
-
-  renderCard = (index, _, cursor) => {
-    const { page } = this.state
-    const item = data[page + index]
-    const alphabet = ['D', 'E', 'G', 'J'];
-
-    return (
-      <div key={index} className='carousel-card'>
-        <div className='alphabet'>
-          <ul>
-            {alphabet && alphabet.length
-              && alphabet.map((letter, k) => (
-                <li key={k}> {letter} </li>
-              ))}
-          </ul>
-        </div>
-        <div className='carousel-card-inner'>
-          <div className='carousel-title'>{item.title}</div>
-          <div className='carousel-text'>{item.text}</div>
-        </div>
-      </div>
-    )
-  }
-
   render() {
-    const { renderedData } = this.state
-    const CarouselContainer = this.container
     return (
-      <div id='screen-list' className='screen'>
-        <Header></Header>
-        <div className='content'>
-          <TouchCarousel
-            ref={elt => { this.carousel = elt }}
-            component={CarouselContainer}
-            cardSize={cardSize}
-            cardCount={renderedData.length}
-            loop={false}
-            renderCard={this.renderCard}
-            defaultCursor={this.defaultCursor}
-          />
+      <div className='screen' id='list-screen'>
+        <Header />
+        <div className='content list'>
+          <CarouselProvider
+            naturalSlideWidth={100}
+            naturalSlideHeight={150}
+            totalSlides={3}
+            visibleSlides={1}
+            touchEnabled={true}
+            dragEnabled={true}
+            infinite='true'
+          >
+            <Slider>
+              <Slide className='slide' index={0}>
+                <div className='slide-item-page s1'>
+                  <div className='alphabet'>
+                    <div className='list-letters'>
+                      <Dot className='letter' slide={0}>
+                        <span></span>
+                      </Dot>
+                      <Dot className='letter' slide={0}>
+                        <span>D</span>
+                      </Dot>
+                      <ButtonNext className='letter'>
+                        <span>E</span>
+                      </ButtonNext>
+                    </div>
+                  </div>
+                  <p>
+                    Teste 1
+                  </p>
+                </div>
+              </Slide>
+              <Slide index={1}>
+                <div className='slide-item-page s2'>
+                  <div className='alphabet'>
+                    <div className='list-letters'>
+                      <ButtonBack className='letter'>
+                        <span>D</span>
+                      </ButtonBack>
+                      <Dot className='letter' slide={1}>
+                        <span>E</span>
+                      </Dot>
+                      <ButtonNext className='letter'>
+                        <span>G</span>
+                      </ButtonNext>
+                    </div>
+                  </div>
+                  <p>
+                    Teste 2
+                  </p>
+                </div>
+              </Slide>
+              <Slide index={2}>
+                <div className='slide-item-page s3'>
+                  <div className='alphabet'>
+                    <div className='list-letters'>
+                      <ButtonBack className='letter'>
+                        <span>G</span>
+                      </ButtonBack>
+                      <Dot className='letter' slide={1}>
+                        <span>J</span>
+                      </Dot>
+                      <ButtonNext className='letter'>
+                        <span>L</span>
+                      </ButtonNext>
+                    </div>
+                  </div>
+                  <p>
+                    Teste 3
+                  </p>
+                </div>
+              </Slide>
+            </Slider>
+          </CarouselProvider>
         </div>
       </div>
-    )
+    );
   }
 }
+
 export default List;
-// document.addEventListener('DOMContentLoaded', function () {
-//   const ndRoot = document.getElementById('react-root')
-//   render(<App />, ndRoot)
-// }

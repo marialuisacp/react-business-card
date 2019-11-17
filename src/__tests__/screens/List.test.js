4c742f3e39b0a722d176a0fa4f65818dea0e7d5b
Maria Luísa <marialuisa@MacBook-Air-de-Maria.local>
2019-11-17 17:05:31 -0300

    Testes automatizados prontos

---
+++ src/__tests__/screens/List.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import List from '../../screens/List/List';
+import { Provider } from 'react-redux'
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe('List', () => {
+  it('renders without crashing given the required props', () => {
+    const props = {
+      letterCenter: 'D',
+      dispatch: jest.fn(),
+      direction: 1
+    }
+
+    const store = mockStore({
+      currentLetter: 'D',
+      direction: 0
+    })
+
+    const wrapper = shallow(<Provider store={store}>
+      <List {...props} />
+    </Provider>)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+})
